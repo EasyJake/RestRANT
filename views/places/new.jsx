3b419cf45c6666dae1e9fcb4a059f37dccb95245
EasyJake <EasyJake@users.noreply.github.com>
2023-11-24 01:44:24 -0800

    Form fields in place

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,8 +7,31 @@ function New() {
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
-                    {/* Form inputs will go here */}
-                </form>
+  <div>
+    <label htmlFor="name">Place Name</label>
+    <input id="name" name="name" required />
+  </div>
+  <div>
+    <label htmlFor="pic">Place Image URL</label>
+    <input id="pic" name="pic" />
+  </div>
+  <div>
+    <label htmlFor="cuisines">Cuisines</label>
+    <input id="cuisines" name="cuisines" required />
+  </div>
+  <div>
+    <label htmlFor="city">City</label>
+    <input id="city" name="city" required />
+  </div>
+  <div>
+    <label htmlFor="state">State</label>
+    <input id="state" name="state" required />
+  </div>
+  <div>
+    <input type="submit" value="Add Place" />
+  </div>
+</form>
+
 
                 <h2>new.jsx</h2>
             </main>
