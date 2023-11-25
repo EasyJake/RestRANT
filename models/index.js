201503efba51ddef246099bf8b48ca5f03a7d373
EasyJake <EasyJake@users.noreply.github.com>
2023-11-25 13:43:20 -0800

    Completed seeder step of Part 9

--- models/index.js
+++ models/index.js
@@ -1,9 +1,17 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Import Mongoose to work with MongoDB
 const mongoose = require('mongoose');
 
+// Connect to MongoDB using the URI provided in the environment variables
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  useNewUrlParser: true, // Use the new URL parser for MongoDB connection strings
+  useUnifiedTopology: true // Use the unified topology for MongoDB connections
 });
 
-module.exports.Place = require('./place');  // Assuming you have a Place model.
-// ... export other models as needed.
+// Export the Place model from the place.js file
+module.exports.Place = require('./place'); // Assuming you have a Place model defined
+
+// Export other models as needed
+// module.exports.OtherModel = require('./otherModel');
--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -1,16 +1,39 @@
-const db = require('../models')
+// Import the models from the models directory. This is required to interact with the database.
+const db = require('../models');
 
+// Define an array of place objects to seed the database. Each object contains fields that match the Place schema.
 const places = [
-  // Add your seed data here
-]
+  {
+    name: 'H-Thai-ML', // The name of the place
+    city: 'Seattle', // The city where the place is located
+    state: 'WA', // The state where the place is located
+    cuisines: 'Thai, Pan-Asian', // The types of cuisines the place offers
+    pic: '/images/h-thai-ml-tables.jpg', // The picture url for the place
+    founded: 1989 // The year the place was founded
+  }, 
+  {
+    name: 'Coding Cat Cafe', // The name of another place
+    city: 'Phoenix', // The city where the second place is located
+    state: 'AZ', // The state where the second place is located
+    cuisines: 'Coffee, Bakery', // The types of cuisines the second place offers
+    pic: '/images/coffee-cat.jpg', // The picture url for the second place
+    founded: 2020 // The year the second place was founded
+  }
+];
 
+// Remove all existing documents from the Place collection before seeding.
 db.Place.deleteMany({})
-  .then(() => db.Place.create(places))
   .then(() => {
-    console.log('Successfully seeded the database.')
-    process.exit()
+    // Seed the database with the new place objects defined above.
+    return db.Place.create(places);
+  })
+  .then(() => {
+    // Log a success message to the console if the database was seeded successfully.
+    console.log('Success! The places have been added to the database.');
+    process.exit(); // Exit the node process successfully.
   })
   .catch(err => {
-    console.error('Seeding failed:', err)
-    process.exit(1)
-  })
+    // Log an error message to the console if the seeding failed.
+    console.error('Failure! An error occurred while seeding the database:', err);
+    process.exit(1); // Exit the node process with an error status.
+  });
