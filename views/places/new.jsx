31d15a9ffbe18b345ab44088e1ee8cc6b7983d6a
EasyJake <EasyJake@users.noreply.github.com>
2023-12-16 16:36:18 -0800

    Added data input validation

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -2,19 +2,16 @@ const React = require('react');
 const Def = require('../default');
 
 function New() {
+    const currentYear = new Date().getFullYear();
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
-                {/* The form tag defines a form that is submitted to the server */}
                 <form method="POST" action="/places">
-                    {/* The fieldset groups related elements in a form */}
                     <fieldset>
                         <legend>Add a New Place</legend>
 
-                        {/* The label represents a caption for an item in a user interface */}
                         <label htmlFor="name">Place Name</label>
-                        {/* The input represents a typed data field, usually with a form control to allow the user to edit the data */}
                         <input id="name" name="name" required />
 
                         <label htmlFor="pic">Place Image URL</label>
@@ -28,12 +25,22 @@ function New() {
 
                         <label htmlFor="state">State</label>
                         <input id="state" name="state" />
+                        
+                        <label htmlFor="founded">Founded Year</label>
+                        <input 
+                            type="number" 
+                            id="founded" 
+                            name="founded" 
+                            required 
+                            min="1600"  // Adjust the year as needed
+                            max={currentYear} 
+                            defaultValue={currentYear} 
+                        />
 
-                        {/* The button with type="submit" submits the form to the server */}
                         <button type="submit">Add Place</button>
                     </fieldset>
                 </form>
-              <h2>views/places/new.jsx</h2>
+                <h2>views/places/new.jsx</h2>
             </main>
         </Def>
     );
